a3a64e5d3b9cbf049a3d8396b29cb249899986b5
semook <xoxosmj@gmail.com>
2024-07-30 16:26:28 +0900

    pr test

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
+        System.out.println("hello5");
 
     }
 }
