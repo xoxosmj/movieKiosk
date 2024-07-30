444bf28dd8ff9d260678c0900cafba536e8066a4
Dancingtuna <paul9119298@gmail.com>
2024-07-30 12:23:29 +0900

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import lombok.Getter;
+
+@Getter
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/member/MemberService.java
@@ -0,0 +1,4 @@
+package member;
+
+public class MemberService {
+}
